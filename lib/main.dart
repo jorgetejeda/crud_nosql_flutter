017138b3f0211ba8aa7f77f9f81dd89eef378eb3
Jorge Tejeda <jorgetejeda0804@gmail.com>
2020-08-11 16:11:28 -0400

    create form to insert student

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'database.helper.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,13 +17,64 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final DatabaseHelper databaseHelper = new DatabaseHelper();
+  final _nameController = TextEditingController();
+  final _courseController = TextEditingController();
+  final _formKey = new GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text('Sqllite CRUD'),
       ),
+      body: ListView(
+        children: [
+          Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: new InputDecoration(labelText: 'Name'),
+                    controller: _nameController,
+                    validator: (val) =>
+                        val.isNotEmpty ? null : 'Nombre no debe estar vacio',
+                  ),
+                  TextFormField(
+                    decoration: new InputDecoration(labelText: 'Curso'),
+                    controller: _courseController,
+                    validator: (val) =>
+                        val.isNotEmpty ? null : 'Curso no debe estar vacio',
+                  ),
+                  RaisedButton(
+                      textColor: Colors.white,
+                      color: Colors.blueAccent,
+                      child: Container(
+                          width: width,
+                          child: Text(
+                            'Guardar',
+                            textAlign: TextAlign.center,
+                          )),
+                      onPressed: () {
+                        _submitStudent(context);
+                      })
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
+
+  void _submitStudent(BuildContext context) {}
 }
